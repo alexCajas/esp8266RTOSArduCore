13653e6408f839bfe5109084efe76d4d4ab43293
alex cajas <alexcajas202@gmail.com>
2023-10-26 14:52:42 +0200

    v1.0.0: Issue in main.cpp, loop() was no running on his task, cheeck again WiFiClientSecure library

--- cores/esp8266/main.cpp
+++ cores/esp8266/main.cpp
@@ -1,12 +1,26 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
 #include "Arduino.h"
+
+#ifndef CONFIG_ARDUINO_LOOP_STACK_SIZE
+#define CONFIG_ARDUINO_LOOP_STACK_SIZE 4096
+#endif
+
+TaskHandle_t loopTaskHandle = NULL;
+void loopTask(void *pvParameters)
+{
+    setup();
+    while(true) {
+        loop();
+        if (serialEventRun) serialEventRun();
+        vTaskDelay(1); // yield cpu to avoid watchdog timeout
+    }
+}
 
 
 extern "C" void app_main()
 {   
-    setup();
-    while (true){
-        loop();
-    }
-    
+ xTaskCreateUniversal(loopTask, "loopTask", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 1, &loopTaskHandle, 0);
 }
 
